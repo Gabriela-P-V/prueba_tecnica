d396af023cc95e94eac1b155a50a41cb6fa53142
Gabriela-P-V <L18100212@nlaredo.tecnm.mx>
2023-05-12 15:08:41 -0600

    subiendo todos los archivos

---
+++ client/src/components/TasksList.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+
+export function TasksList() {
+    useEffect(() => {
+        console.log("pagina ok")
+      }, []);
+
+
+  return (
+    <div>TasksList</div>
+  )
+}
+
